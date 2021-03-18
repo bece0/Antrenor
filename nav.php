3d55312568e741e6bc95e68b66ea8325750b1190
bece0 <bgmclb45@gmail.com>
2021-03-19 02:32:40 +0300

    navbarda bir sorun çözüldü

--- nav.php
+++ nav.php
@@ -5,18 +5,23 @@
   //  var_dump($kullanici_giris_yapti_mi);
 ?>
 
+<div class="navbar-fixed">
 <nav>
     <div class="nav-wrapper teal lighten-2 ">
-        <a href="index.php" class="brand-logo"> <img src="files/images/logo.png"
-                style="margin-bottom:1px;border-radius:1000px;width:5%"></a>
+        <a href="index.php" class="brand-logo "> <img src="files/images/logo.png"  style="margin-left:10px;border-radius:1000px;width:60px"> </a>  
+
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="index.php">Sporcu Listesi</a></li>
+
+            <li class="active"><a href="index.php">Sporcu Listesi</a></li>
             <li><a href="puan.php">Puan Tablosu</a></li>
             <li><a href="aidat.php">Aidat Tablosu</a></li>
-           
+
             <?php if($kullanici_giris_yapti_mi ){ ?>
             <li><a class="waves-effect waves-light btn cyan darken-4" href="login.php">Çıkış</a></li>
             <?php } ?>
+
         </ul>
+
     </div>
 </nav>
+</div>
