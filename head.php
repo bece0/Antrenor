b069254581e40bb2401fb3ff398c2fc3079e29ef
bece0 <bgmclb45@gmail.com>
2021-04-15 23:33:03 +0300

    yeni dosyalar eklendi

--- head.php
+++ head.php
@@ -7,6 +7,8 @@
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"> </script>  -->
       
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
@@ -19,7 +21,7 @@
          <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.js"></script> -->
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css"> -->
         <!-- <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"> </script> -->
+       
         <!-- <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script> -->
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script> -->
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"> -->
---
+++ modals/yarisma_ekle_modal.php
@@ -0,0 +1,85 @@
+<?php 
+    include 'head.php';
+    include 'nav.php';
+
+?>
+
+<body>
+
+    <!-- Modal Structure -->
+    <div id="yarisma_ekle_modal" class="modal modal-fixed-footer">
+        <div class="modal-content">
+
+            <br>
+
+            <h5 style="text-align:center"> Yarışma Bilgileri</h5>
+            <form action="action/yarisma_ekle_action.php" method="post" id="yarisma_ekle">
+                <table>
+                    <thead>
+                        <tr>
+                            <th data-field="1"></th>
+                            <th data-field="2"></th>
+                            <th data-field="3"></th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td style="text-align:center"><i class="material-icons">local_activity</i></td>
+                            <td> Yarışma Adı :</td>
+                            <td> <input name="yarisma_adi" type="text" class="validate"> </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style="text-align:center"><i class="material-icons">event</i></td>
+                            <td> Tarih :</td>
+                            <td> <input name="tarih" type="text" class="validate"> </td>
+
+                        </tr>
+                        <tr>
+
+                            <td style="text-align:center"><i class="material-icons">storage</i></td>
+                            <td> Sıralama :</td>
+                            <td> <input name="siralama" type="text" class="validate"> </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td style="text-align:center"><i class="material-icons">stars</i></td>
+                            <td> Madalya :</td>
+                            <td> <input name="madalya" type="text" class="validate"> </td>
+
+                        </tr>
+
+
+                    </tbody>
+
+                </table>
+                <br>
+
+
+
+
+        </div>
+        <div class="modal-footer">
+
+            <center>
+                <input type="hidden" id="sporcu_no" name="sporcu_no" value="<?php echo $sporcu_no ?>" />
+                <button type="submit" class=" modal-close waves-light btn green" id="yarisma_ekle_buton"
+                    form="yarisma_ekle">TAMAMLA</a>
+            </center>
+           
+        </div>
+        </form>
+    </div>
+
+
+
+
+
+
+</body>
